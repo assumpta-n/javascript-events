ed118183f67fb8bc08753d9c3e58b0932a41e1d9
Assumpta <assumptanalubowa@gmail.com>
2024-06-19 17:22:06 +0300

    Listens to form input's blur event using the onblur event Handler Javascript property

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function useHandlerAttribute() {
-    console.log("We are using an onclick, event handler, HTML attribute to listen to a click event on this button ")
+function logTextToConsole() {
+    console.log("We are using an onclick, HTML event handler attribute to listen to a click event on this button ")
 }
+
+function showAlertOnBlur() {
+    alert("The input is out of focus. This means we've triggered a blur event")
+}
+
+const usernameInput = document.getElementById('userName')
+usernameInput.onblur = showAlertOnBlur;
