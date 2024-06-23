9f746eec040c6a07c3d7c2e8e01caf4904fa7f4c
Assumpta <assumptanalubowa@gmail.com>
2024-06-23 15:31:45 +0300

    Uses global event object without passing it to Inline Event Handler

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function logTextToConsole(myEvent) {
+function logTextToConsole() {
     console.log("We are using an onclick, HTML event handler attribute to listen to a click event on this button ")
-    console.log("The event is", myEvent)
-    console.log("The event type is", myEvent)
+    console.log("The event is", event) //You can access the global Javascript event object directly.
+    console.log("The event type is", event.type)
 }
+
+
+
 
 // function showAlertOnBlur() {
 //     alert("The input is out of focus. This means we've triggered a blur event")
