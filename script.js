0c6c659f50d7107e684cbd38ba55f76ba5b461d6
Assumpta <assumptanalubowa@gmail.com>
2024-06-23 17:06:27 +0300

    Removes inline event handler attribute

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-function logTextToConsole() {
+function logTextToConsole(myEvent) {
     console.log("We are using an onclick, HTML event handler attribute to listen to a click event on this button ")
-    console.log("The event is", event) //You can access the global Javascript event object directly.
-    console.log("The event type is", event.type)
+    console.log("The event is", myEvent) //You can access the global Javascript event object directly.
+    console.log("The event type is", myEvent.type)
 }
 
-
+function stopLoggingTextToConsole() {
+    const logTextBtn = document.getElementById('logTextBtn')
+    logTextBtn.removeAttribute("onclick")
+    alert("The Log Text Button doesn't work anymore. Click it to find out for yourself")
+}
 
 
 // function showAlertOnBlur() {
